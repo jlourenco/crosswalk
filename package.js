df8c5f7630fa74b22e571bdc23eed26f30f70c16
Chun Yang <yang2007chun@gmail.com>
2016-02-25 15:21:00 -0600

    1.4.0

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  name: 'poetic:crosswalk',
+  version: '1.4.0',
+  summary: "This crosswalk version (1.4.0) fix the android camera input bug and is compatible with meteor 1.2.1",
+  documentation: null
+});
+
+Cordova.depends({
+  'cordova-plugin-crosswalk-webview': '1.4.0'
+});
