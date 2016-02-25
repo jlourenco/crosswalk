b8be85d29c56af77128dc91c9523e1701d61e40b
Chun Yang <yang2007chun@gmail.com>
2016-02-25 15:27:14 -0600

    1.4.0_1

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
   name: 'poetic:crosswalk',
-  version: '1.4.0',
+  version: '1.4.0_1',
   summary: "This crosswalk version (1.4.0) fix the android camera input bug and is compatible with meteor 1.2.1",
+  git: 'https://github.com/poetic/crosswalk',
   documentation: null
 });
 
