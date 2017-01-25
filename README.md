4f6c1646d3311255def2786c00aceaaf98319b3a
João Lourenço <jlourenco@pdmfc.com>
2017-01-25 14:07:36 +0000

    crosswalk version downgrade to 2.1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+Meteor package that wraps Crosswalk 2.1.0
+
+meteor add jlourenco:crosswalk
--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: 'jlourenco:crosswalk',
   version: '0.0.1',
-  summary: 'Crosswalk 2.3.0',
+  summary: 'Crosswalk 2.1.0',
   git: 'https://github.com/jlourenco/crosswalk',
   documentation: 'README.md'
 });
 
 Cordova.depends({
-  'cordova-plugin-crosswalk-webview': '2.3.0'
+  'cordova-plugin-crosswalk-webview': '2.1.0'
 });
