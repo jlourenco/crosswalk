3e930604e2f6f155392ef023f29a5e73ee14794a
João Lourenço <jlourenco@pdmfc.com>
2017-01-25 11:49:23 +0000

    crosswalk version updated to 2.3.0

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
-  name: 'poetic:crosswalk',
-  version: '1.4.0_1',
-  summary: "This crosswalk version (1.4.0) fix the android camera input bug and is compatible with meteor 1.2.1",
-  git: 'https://github.com/poetic/crosswalk',
-  documentation: null
+  name: 'jlourenco:crosswalk',
+  version: '0.0.1',
+  summary: 'Crosswalk 2.3.0',
+  git: 'https://github.com/jlourenco/crosswalk',
+  documentation: 'README.md'
 });
 
 Cordova.depends({
-  'cordova-plugin-crosswalk-webview': '1.4.0'
+  'cordova-plugin-crosswalk-webview': '2.3.0'
 });
